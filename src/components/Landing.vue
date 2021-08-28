5b635619584979c95ace1f2190c46db0fe9dd0ef
Civan <civan.erbay@web.de>
2021-08-28 17:02:47 +0200

    Init new civan.io project

---
+++ src/components/Landing.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{content.title}}</h1>
+  <img alt="civan erbay" class="landing-image" src="../assets/civan_erbay.png" /> 
+  <button type="button" @click="count++">count is: {{ count }}</button>
+</template>
+
+<script>
+  import contentJSON from '../content.js'
+  export default {
+    name: 'Landing',
+    data() {
+      return {
+        count: 0,
+        content: contentJSON,
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .landing-image {
+    height: 50px;
+
+  }
+</style>
